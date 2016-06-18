99b7fc4410cc74daa6ad9250e198d963f3204c92
David Barnett <david@barnett.net.nz>
2016-06-19 11:28:15 +1200

    enable template inheritance & file watching

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate iron;
 extern crate handlebars_iron as hbs;
+extern crate handlebars;
 extern crate router;
 extern crate mount;
 extern crate staticfile;
 extern crate rustc_serialize;
+extern crate env_logger;
 
 use iron::prelude::*;
 use iron::status;
@@ -11,33 +13,53 @@ use router::Router;
 use staticfile::Static;
 use mount::Mount;
 use hbs::{Template, HandlebarsEngine, DirectorySource};
+use handlebars::Handlebars;
 use rustc_serialize::json::{Json, ToJson};
+
+#[cfg(feature = "watch")]
+use hbs::Watchable;
 
 use std::path::Path;
 use std::collections::BTreeMap;
 use std::error::Error;
+use std::sync::Arc;
 
 fn index(req: &mut Request) -> IronResult<Response> {
-    println!("Running send_hello handler, URL path: {:?}", req.url.path);
+    println!("URL path: {:?}", req.url.path);
     let mut res = Response::new();
     let data = BTreeMap::<String, Json>::new();
-    println!("{:?}", data);
-    res.set_mut(Template::new("layout", data)).set_mut(status::Ok);
-    println!("wow {:?}", res);
+    res.set_mut(Template::new("index", data))
+        .set_mut(status::Ok);
     Ok(res)
 }
 fn error_404(_: &mut Request) -> IronResult<Response> {
     let mut res = Response::new();
-    let data = BTreeMap::<String, Json>::new();
-    res.set_mut(Template::new("error", data)).set_mut(status::NotFound);
+    let mut data = BTreeMap::<String, Json>::new();
+    data.insert("error".to_owned(), "Page not found".to_json());
+    res.set_mut(Template::new("error", data))
+        .set_mut(status::NotFound);
     Ok(res)
 }
 
+#[cfg(feature = "watch")]
+fn watch_hbs(hbs: Arc<HandlebarsEngine>) -> Arc<HandlebarsEngine> {
+    hbs.watch("views");
+    hbs
+}
+
+#[cfg(not(feature = "watch"))]
+fn watch_hbs(hbs: Arc<HandlebarsEngine>) -> Arc<HandlebarsEngine> {
+    hbs
+}
+
 fn main() {
-    let mut hbse = HandlebarsEngine::new();
+    env_logger::init().unwrap();
+    let mut hb = Handlebars::new();
+    hb.register_template_file("layout", &Path::new("views/layout.hbs")).ok().unwrap();
+    let mut hbse = HandlebarsEngine::from(hb);
 
     // hbse.register_template_file("template", "../views/layout.hbs");
-    hbse.add(Box::new(DirectorySource::new("../views", ".hbs")));
+    hbse.add(Box::new(DirectorySource::new("views", ".hbs")));
 
     // load templates from all registered sources
     if let Err(r) = hbse.reload() {
@@ -45,15 +67,16 @@ fn main() {
     }
 
     let mut router = Router::new();
-    router.get("/", index); 
+    router.get("/", index);
+    router.any("**", error_404);
 
     let mut mount = Mount::new();
-    mount.mount("/public/", Static::new(Path::new("../public")))
-         .mount("/", router);
+    mount.mount("/public/", Static::new(Path::new("public")))
+        .mount("/", router);
 
 
     let mut chain = Chain::new(mount);
-    chain.link_after(hbse);
+    chain.link_after(watch_hbs(Arc::new(hbse)));
 
     println!("Server running at http://localhost:3000/");
     Iron::new(chain).http("localhost:3000").unwrap();
